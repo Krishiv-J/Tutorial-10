c222aa19979249e88e9ddbaacbeffbd2dbd61360
Krishiv-J <krishivjain1234@gmail.com>
2024-03-18 16:41:15 -0400

    Model Script

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -9,22 +9,20 @@
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(dplyr)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data <- read_csv("data/analysis_data/cleaned_data.csv")
 
 ### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
 
+# From https://tellingstorieswithdata.com/13-ijaglm.html
+
+model <- 
+  glm(goals_conceded ~ away_attacking_strength + away_defending_strength,
+      data = analysis_data, family = "poisson")
+
+summary(model)
 
 #### Save model ####
 saveRDS(
@@ -32,4 +30,5 @@ saveRDS(
   file = "models/first_model.rds"
 )
 
+analysis_data <- mutate(analysis_data, predicted_goals = predict(model, newdata = analysis_data, type = "response"))
 
